78ea61c3326efa0a76097769c47456fbc8513441
Andrei <andreyned@gmail.com>
2017-07-13 01:59:06 +0300

    добавил материалы 7-го занятия в репозиторий

---
+++ src/by/htp/lsn7b/Student.java
@@ -0,0 +1,30 @@
+package by.htp.lsn7b;
+
+public class Student {
+	
+	private char[] name;
+	
+	
+
+	public Student() {
+		
+	}
+
+	public Student(char[] name) {
+		
+		this.name = name;
+	}
+
+
+
+	public char[] getName() {
+		return name;
+	}
+
+	public void setName(char[] name) {
+		this.name = name;
+	}
+	
+	
+
+}
